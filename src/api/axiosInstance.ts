e84dd0ca452b2ffc7b89765f226927d820374b45
rajtirole <rtech23456@gmail.com>
2025-11-24 00:13:13 +0530

    chore test

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  //  baseURL: "https://54.174.229.126:5000/api",
+  // baseURL: "http://localhost:5000/api",
+   baseURL: "http://54.174.229.126:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
